61f418647fade34e50c0c54a1e329eed5c1e22ed
uonce0 <ywsong000@gmail.com>
2026-01-30 16:28:36 +0900

    github actions test:error fix

--- src/main/java/springboot/giftledger/GiftLedgerApplication.java
+++ src/main/java/springboot/giftledger/GiftLedgerApplication.java
@@ -2,6 +2,7 @@ package springboot.giftledger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GiftLedgerApplication {
